d6c9c0f65f2499180874b032ed8103956fbfa81b
Linoy Pappachan Malakkaran <malakkaran.pappachan@maqta.ae>
2022-08-05 01:21:55 +0400

    fixes

--- projects/shell/src/app/home/home.component.ts
+++ projects/shell/src/app/home/home.component.ts
@@ -1,11 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { loadRemoteModule } from '@angular-architects/module-federation';
+import { ChangeDetectorRef, Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  @ViewChild('vc', { read: ViewContainerRef, static: false })
+  viewContainer: ViewContainerRef | undefined;
 
-  ngOnInit() {}
+  constructor(private ref: ChangeDetectorRef) { }
+
+  ngOnInit() {
+    this.loadMfeWidget();
+  }
+
+  async loadMfeWidget(): Promise<void> {
+    const m = await loadRemoteModule({
+      type: 'module',
+      remoteEntry: "http://localhost:5203/remoteEntry.js",
+      exposedModule: './Component'
+    });
+    const ref = this.viewContainer!.createComponent(
+      m['SampleBarChartComponent']
+    );
+
+    // const compInstance = ref.instance;
+    setInterval(() => {
+      this.ref.markForCheck();
+    }, 1000);
+  }
 }
